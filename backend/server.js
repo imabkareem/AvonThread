70da93cec7277f06ce46d4f96b2f60c6c9e119e5
Abdul Kareem <158294569+imabkareem@users.noreply.github.com>
2025-08-28 16:32:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
-const path = require('path');
 const connectDB = require("./config/db")
 const  userRoutes = require("./routes/userRoutes")
 const productRoutes = require("./routes/productRoutes")
